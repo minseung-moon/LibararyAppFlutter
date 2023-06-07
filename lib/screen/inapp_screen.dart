971a4d95ae8bf864fa2c6e253320f84f443e38ad
minseung-moon <ansalstmd@gmail.com>
2023-06-07 20:01:37 +0900

    로고 수정 및 바코드 수정 파일 접근 허용

--- lib/screen/inapp_screen.dart
+++ lib/screen/inapp_screen.dart
@@ -81,7 +81,7 @@ class _InAppWebViewScreenState extends State<InAppScreen> {
 
     try {
       barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-          '#ff6666', 'Cancel', true, ScanMode.BARCODE);
+          '#00000000', 'Cancel', false, ScanMode.BARCODE);
       print(barcodeScanRes);
     } on PlatformException {
       barcodeScanRes = 'Failed to get platform version.';
@@ -89,25 +89,24 @@ class _InAppWebViewScreenState extends State<InAppScreen> {
 
     if (!mounted) return;
 
-    //String url = "http://dandi.15449642.com?isbn=" + barcodeScanRes;
-    String url = "http://applibrary2023.15449642.com:8080/main/site/appLibrary/search.do?";
-    url += "cmd_name=bookandnonbooksearch";
-    url += "&search_type=detail";
-    url += "&detail=OK";
-    url += "&use_facet=N";
-    url += "&manage_code=MS%2CMB%2CMC%2CMG%2CMA%2CMJ%2CMH%2CMN%2CMO%2CMP%2CMQ%2CMR%2CMK%2CML%2CME%2CMF%2CMT%2CMU%2CMV%2CMW%2CMX%2CNA";
-    url += "&all_lib=N";
-    url += "&all_lib_detail_big=Y";
-    url += "&all_lib_detail_small=Y";
-    url += "&search_isbn_issn=" + barcodeScanRes;
+    if(barcodeScanRes != "-1") {
+      //String url = "http://dandi.15449642.com?isbn=" + barcodeScanRes;
+      String url = "http://applibrary2023.15449642.com:8080/main/site/appLibrary/search.do?";
+      url += "cmd_name=bookandnonbooksearch";
+      url += "&search_type=detail";
+      url += "&detail=OK";
+      url += "&use_facet=N";
+      url += "&manage_code=MS%2CMB%2CMC%2CMG%2CMA%2CMJ%2CMH%2CMN%2CMO%2CMP%2CMQ%2CMR%2CMK%2CML%2CME%2CMF%2CMT%2CMU%2CMV%2CMW%2CMX%2CNA";
+      url += "&all_lib=N";
+      url += "&all_lib_detail_big=Y";
+      url += "&all_lib_detail_small=Y";
+      url += "&search_isbn_issn=" + barcodeScanRes;
 
-    print('barcode code value');
-    print(url);
-
-    if (webViewController != null) {
-      await webViewController.loadUrl(urlRequest: URLRequest(
-        url: Uri.parse(url),
-      )); // Replace with your new Korean URL
+      if (webViewController != null) {
+        await webViewController.loadUrl(urlRequest: URLRequest(
+          url: Uri.parse(url),
+        )); // Replace with your new Korean URL
+      }
     }
   }
   Future<String?> getDeviceUniqueId() async {
