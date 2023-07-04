0bb946dc955efa108a3bf64cdcb8fc1b6a7e4be9
minseung-moon <ansalstmd@gmail.com>
2023-07-04 17:18:55 +0900

    shake 오류 수정

--- lib/screen/inapp_screen.dart
+++ lib/screen/inapp_screen.dart
@@ -22,16 +22,22 @@ class InAppScreen extends StatefulWidget {
   State<InAppScreen> createState() => _InAppWebViewScreenState();
 }
 
-class _InAppWebViewScreenState extends State<InAppScreen> {
+class _InAppWebViewScreenState extends State<InAppScreen>  with WidgetsBindingObserver {
   final GlobalKey webViewKey = GlobalKey();
   Uri myUrl = Uri.parse("http://applibrary2023.15449642.com:8080/");
   late final InAppWebViewController webViewController;
   late final PullToRefreshController pullToRefreshController;
   double progress = 0;
+  bool _isShakeDetectorActive = false;
+
+  late ShakeDetector _shakeDetector;
 
   @override
   void initState() {
     super.initState();
+
+    WidgetsBinding.instance.addObserver(this);
+    _isShakeDetectorActive = true;
 
     WidgetsBinding.instance.addPersistentFrameCallback((timeStamp) async {
       if(Platform.isAndroid) {
@@ -50,11 +56,59 @@ class _InAppWebViewScreenState extends State<InAppScreen> {
           webViewController.loadUrl(urlRequest: URLRequest(url: await webViewController.getUrl()));}
       },
     ))!;
-    
-    ShakeDetector.autoStart(onPhoneShake: () {
-      //webViewController.evaluateJavascript(source: "alert('hi')");
-      scanBarcodeNormal();
+
+    _shakeDetector = ShakeDetector.autoStart(onPhoneShake: () {
+      if(_isShakeDetectorActive) {
+        //webViewController.evaluateJavascript(source: "alert('hi')");
+        scanBarcodeNormal();
+      }
     });
+    _shakeDetector.startListening();
+  }
+
+  @override
+  void dispose() {
+    //앱 상태 변경 이벤트 해제
+    //문제는 앱 종료시 dispose함수가 호출되지 않아 해당 함수를 실행 할 수가 없다.
+    _isShakeDetectorActive = false;
+    _shakeDetector.stopListening();
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // 앱 상태 변경시 호출
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    switch (state) {
+      case AppLifecycleState.resumed:
+      // 앱이 표시되고 사용자 입력에 응답합니다.
+      // 주의! 최초 앱 실행때는 해당 이벤트가 발생하지 않습니다.
+        _isShakeDetectorActive = true;
+        _shakeDetector.startListening();
+        break;
+      case AppLifecycleState.inactive:
+      // 앱이 비활성화 상태이고 사용자의 입력을 받지 않습니다.
+      // ios에서는 포 그라운드 비활성 상태에서 실행되는 앱 또는 Flutter 호스트 뷰에 해당합니다.
+      // 안드로이드에서는 화면 분할 앱, 전화 통화, PIP 앱, 시스템 대화 상자 또는 다른 창과 같은 다른 활동이 집중되면 앱이이 상태로 전환됩니다.
+      // inactive가 발생되고 얼마후 pasued가 발생합니다.
+        _isShakeDetectorActive = false;
+        _shakeDetector.stopListening();
+        break;
+      case AppLifecycleState.paused:
+      // 앱이 현재 사용자에게 보이지 않고, 사용자의 입력을 받지 않으며, 백그라운드에서 동작 중입니다.
+      // 안드로이드의 onPause()와 동일합니다.
+      // 응용 프로그램이 이 상태에 있으면 엔진은 Window.onBeginFrame 및 Window.onDrawFrame 콜백을 호출하지 않습니다.
+        _isShakeDetectorActive = false;
+        _shakeDetector.stopListening();
+        break;
+      case AppLifecycleState.detached:
+      // 응용 프로그램은 여전히 flutter 엔진에서 호스팅되지만 "호스트 View"에서 분리됩니다.
+      // 앱이 이 상태에 있으면 엔진이 "View"없이 실행됩니다.
+      // 엔진이 처음 초기화 될 때 "View" 연결 진행 중이거나 네비게이터 팝으로 인해 "View"가 파괴 된 후 일 수 있습니다.
+        _isShakeDetectorActive = false;
+        _shakeDetector.stopListening();
+        break;
+    }
   }
 
   // 밝기조절 (0~1)
